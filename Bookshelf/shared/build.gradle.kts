f52b9c407274aa9517a91eb5a49802bbcca340a9
Eduardo López <eduardo.lopez@mongodb.com>
2021-07-22 12:23:16 +0200

    Removed unused dependency

--- Bookshelf/shared/build.gradle.kts
+++ Bookshelf/shared/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     kotlin("multiplatform")
     id("com.android.library")
     id("kotlin-android-extensions")
-    kotlin("plugin.serialization")
     id("io.realm.kotlin") version "0.4.1"
 }
 
