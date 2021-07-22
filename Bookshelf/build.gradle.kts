2c6430ab821622b7150b4d1fb825caa270f57a5f
Eduardo López <eduardo.lopez@mongodb.com>
2021-07-22 11:26:51 +0200

    Removed unused dependencies

--- Bookshelf/build.gradle.kts
+++ Bookshelf/build.gradle.kts
@@ -7,7 +7,6 @@ buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
         classpath("com.android.tools.build:gradle:7.1.0-alpha02")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.5.20")
     }
 }
 
