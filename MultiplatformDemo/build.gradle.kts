9fab30b92df0bdb13c40dbbb328eacd9e9ffefb2
Nabil Hachicha <nabil.hachicha@mongodb.com>
2021-10-16 15:38:36 +0100

    Update dependencies for Realm Multiplatform Demo

--- MultiplatformDemo/build.gradle.kts
+++ MultiplatformDemo/build.gradle.kts
@@ -3,12 +3,10 @@ buildscript {
         gradlePluginPortal()
         google()
         mavenCentral()
-        maven("https://oss.sonatype.org/content/repositories/snapshots")
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
         classpath("com.android.tools.build:gradle:7.0.2")
-        classpath("io.realm.kotlin:gradle-plugin:0.6.0-SNAPSHOT")
     }
 }
 
@@ -16,7 +14,6 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven("https://oss.sonatype.org/content/repositories/snapshots")
     }
 }
 
