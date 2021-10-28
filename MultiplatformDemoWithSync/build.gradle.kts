7d748a139b32d397122c3ded302415ece520c7b0
Nabil Hachicha <nabil.hachicha@mongodb.com>
2021-10-28 11:23:12 +0100

    wip

--- MultiplatformDemoWithSync/build.gradle.kts
+++ MultiplatformDemoWithSync/build.gradle.kts
@@ -3,8 +3,8 @@ buildscript {
         gradlePluginPortal()
         google()
         mavenCentral()
-        mavenLocal()
-//        maven ("https://oss.sonatype.org/content/repositories/snapshots")
+//        mavenLocal()
+        maven ("https://oss.sonatype.org/content/repositories/snapshots")
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
@@ -17,8 +17,8 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        mavenLocal()
-//        maven ("https://oss.sonatype.org/content/repositories/snapshots")
+//        mavenLocal()
+        maven ("https://oss.sonatype.org/content/repositories/snapshots")
     }
 }
 
