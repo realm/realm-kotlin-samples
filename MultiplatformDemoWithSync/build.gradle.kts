c1443e822192df1c8cd3a1df19b581063f3757f6
Nabil Hachicha <nabil.hachicha@mongodb.com>
2022-01-28 10:42:01 +0000

    Update Sync demo

--- MultiplatformDemoWithSync/build.gradle.kts
+++ MultiplatformDemoWithSync/build.gradle.kts
@@ -3,10 +3,14 @@ buildscript {
         gradlePluginPortal()
         google()
         mavenCentral()
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+        }
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-        classpath("com.android.tools.build:gradle:7.1.0-rc01")
+        classpath("com.android.tools.build:gradle:7.1.0")
+        classpath("io.realm.kotlin:gradle-plugin:0.9.0-SNAPSHOT")
     }
 }
 
@@ -14,10 +18,13 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+        }
     }
 
     group = "io.realm.sample"
-    version = "0.8.2"
+    version = "0.9.0"
 }
 
 tasks.register("clean", Delete::class) {
