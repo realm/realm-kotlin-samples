67eee9439c04bac278a893b5b9c88cd57ac0eaf1
Christian Melchior <christian.melchior@mongodb.com>
2023-05-23 19:41:53 +0200

    Update dependencies

--- MultiplatformDemo/build.gradle.kts
+++ MultiplatformDemo/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.7.20" apply false
+    kotlin("multiplatform") version "1.8.20" apply false
     id("com.android.library") version "7.3.0" apply false
 }
 
