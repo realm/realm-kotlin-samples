e4767444d63dc87d3a40c4b606915c81318c401f
Clemente <clemente.tort@mongodb.com>
2023-06-07 01:01:06 +0200

    Add copyright

--- CuratedSyncExamples/app/src/main/java/io/realm/curatedsyncexamples/fieldencryption/FieldEncryptionActivity.kt
+++ CuratedSyncExamples/app/src/main/java/io/realm/curatedsyncexamples/fieldencryption/FieldEncryptionActivity.kt
@@ -1,12 +1,26 @@
+/*
+ * Copyright 2023 Realm Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.realm.curatedsyncexamples.fieldencryption
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.lifecycle.ViewModelProvider
 import io.realm.curatedsyncexamples.fieldencryption.ui.NavGraph
 import io.realm.curatedsyncexamples.ui.theme.CuratedSyncExamplesTheme
-import io.realm.kotlin.mongodb.App
 
 class FieldEncryptionActivity : ComponentActivity() {
 
--- CuratedSyncExamples/app/src/main/java/io/realm/curatedsyncexamples/fieldencryption/ui/records/SecretRecordsViewModel.kt
+++ CuratedSyncExamples/app/src/main/java/io/realm/curatedsyncexamples/fieldencryption/ui/records/SecretRecordsViewModel.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Realm Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.realm.curatedsyncexamples.fieldencryption.ui.records
 
 import androidx.lifecycle.MutableLiveData
@@ -5,8 +21,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import io.realm.curatedsyncexamples.fieldencryption.ext.fieldEncryptionCipherSpec
 import io.realm.curatedsyncexamples.fieldencryption.models.AndroidKeyStoreHelper
-import io.realm.curatedsyncexamples.fieldencryption.models.SecretRecord
 import io.realm.curatedsyncexamples.fieldencryption.models.EncryptedStringField
+import io.realm.curatedsyncexamples.fieldencryption.models.SecretRecord
 import io.realm.curatedsyncexamples.fieldencryption.models.cipherSpec
 import io.realm.curatedsyncexamples.fieldencryption.models.getFieldLevelEncryptionKey
 import io.realm.curatedsyncexamples.fieldencryption.models.key
